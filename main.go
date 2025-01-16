dacc0f698635e51073c58f7af3fcb258f2c644f5
codybrunson <cody.code.brunson@gmail.com>
2025-01-16 08:41:39 -0600

    Some organizing of functions and clean up of the metrics function.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -21,7 +20,7 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", middlewareMetricsInc(cfg, http.StripPrefix("/app", http.FileServer(http.Dir(FILEPATHROOT)))))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(FILEPATHROOT)))))
 	mux.Handle("/metrics/", http.HandlerFunc(cfg.handlerMetrics))
 	mux.Handle("/reset/", http.HandlerFunc(cfg.handlerReset))
 	mux.HandleFunc("/healthz", handlerReadiness)
@@ -35,27 +34,7 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	fileserverHits := cfg.fileserverHits.Load()
-	fmt.Fprintf(w, "Hits: %d\n", fileserverHits)
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	cfg.fileserverHits.Store(0)
-	w.Write([]byte("Hits counter reset to 0"))
-}
-
-func middlewareMetricsInc(cfg *apiConfig, next http.Handler) http.Handler {
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
